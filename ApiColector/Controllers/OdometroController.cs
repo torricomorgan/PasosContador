6ec64b9a49083dd9cdbbb82102561a5d5c9a1105
Matias Torrico <a2016114304@estudiantes.upsa.edu.bo>
2021-03-14 20:32:59 -0400

    Add project files.

---
+++ ApiColector/Controllers/OdometroController.cs
@@ -0,0 +1,42 @@
+﻿using ApiColector.Models;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiColector.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OdometroController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Odometro odometro)
+        {
+
+            string connectionString = "Endpoint=sb://queuematias.servicebus.windows.net/;SharedAccessKeyName=enviar;SharedAccessKey=/NoSg88zhM6W5wCoN6kmMdFNv1hebNZ2V3rSFz21zWA=;EntityPath=ejercicios";
+            string queueName = "ejercicios";
+            string mensaje = JsonConvert.SerializeObject(odometro);
+
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ApiColector/Models/Odometro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiColector.Models
+{
+    public class Odometro
+    {
+        [Key]
+        public string Email { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime DateTime { get; set; }
+        [Required]
+        public int Step { get; set; }
+
+    }
+}
